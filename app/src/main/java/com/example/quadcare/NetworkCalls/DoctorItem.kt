ddfc1f4a2c1d970812d081f31ad07d6fbe4a9623
naveen golla <naveeng@csu.fullerton.edu>
2022-12-09 02:45:43 -0800

    Class Project is pushed to Personal git repo

---
+++ app/src/main/java/com/example/quadcare/NetworkCalls/DoctorItem.kt
@@ -0,0 +1,9 @@
+package com.example.quadcare.NetworkCalls
+
+data class DoctorItem (
+    val id: Int,
+    val name: String,
+    val specializationId: Int,
+    val specialization: String,
+    val phone: String
+    )
